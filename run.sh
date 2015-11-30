000c463ce670d184bc20f40b1d65b5e593860032
Oleg Strizhechenko <oleg@carbonsoft.ru>
2015-12-01 02:23:52 +0500

    Держи рефакторенный вариант
    1. Разбил всё по функциям
    2. Повторяющиеся read -p x if $x = вынес в функцию ask
    3. Избавился от лишних ветвлений
    4. Чуток упростил подготовку к запуску
    
    Советы: избавься от всех этих cd config, cd -, постарайся всё вызывать из текущей директории, дописывая полный путь. Код станет куда чище. От &>/dev/null везде тоже имеет смысл избавиться, лучше сделать так, чтобы команды которые вызываешь вообще с ошибкой не падали.

--- run.sh
+++ run.sh
@@ -1,26 +1,11 @@
 #!/bin/bash
-#v0.05
-cd "$(dirname "$0")"
-#echo "Hey"
-echo "1 - Create config"
-echo "2 - Install config"
-echo "3 - Recover default preferences"
-echo "4 - Clean config folder"
-if [ ! -d config ]; then
-  mkdir config
-fi
-read -p ": " CHOICE
 
-if [ "$CHOICE" -eq 1 ]; then
-  #Checking for sys_pref folder
-  cd config
-  if [ ! -d sys_pref ]; then
-    mkdir sys_pref
-  fi
-  if [ ! -d apps_pref ]; then
-    mkdir apps_pref
-  fi
-  cd - > /dev/null 2>&1
+ask() {
+  read -p "$1" prompt  
+  [ "${prompt:0:1}" = y -o "${prompt:0:1}" = Y ]
+}
+
+create_config() {
   echo "Copying /Library/Preferences/ folder... "
   sudo rsync -av --progress  /Library/Preferences/* config/sys_pref --exclude OpenDirectory > /dev/null 2>&1
   echo "Copying ~/Library/Preferences/ folder... "
@@ -46,78 +31,61 @@ if [ "$CHOICE" -eq 1 ]; then
   cd - > /dev/null 2>&1
   sudo rm -rf config
   echo "Done"
-fi
+}
 
-if [ "$CHOICE" -eq 2 ]; then
-  read -p "Are you sure you want to continue? <y/n> " prompt
-  if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]; then
-    echo "Unpacking..."
-    mv config.tar.gz config > /dev/null 2>&1
-    cd config > /dev/null 2>&1
-    sudo tar -pxzf config.tar.gz > /dev/null 2>&1
-    rm -r config.tar.gz > /dev/null 2>&1
-    echo "Installing..."
-    cd - > /dev/null 2>&1
-    cd config/sys_pref/tmp
-    sudo tar -pxzf preferences.tar.gz > /dev/null 2>&1
-    rm -f preferences.tar.gz
-    sudo rsync -av -I --progress * /Library/Preferences/ > /dev/null 2>&1
-    cd - > /dev/null 2>&1
-    cd config/apps_pref/tmp
-    sudo tar -pxzf apps_preferences.tar.gz > /dev/null 2>&1
-    rm -f apps_preferences.tar.gz
-    sudo rsync -av -I --progress * ~/Library/Preferences/ > /dev/null 2>&1
-    cd - > /dev/null 2>&1
-    cd config > /dev/null 2>&1
-    sudo rsync -av -I --progress login.keychain ~/Library/Keychains/ > /dev/null 2>&1
-    echo "Installed"
-    read -p "Erase config folder? (y/n)" PROMTERASE
-    if [[ $PROMTERASE == "y" || $PROMTERASE == "Y" || $PROMTERASE == "yes" || $PROMTERASE == "Yes" ]]; then
-      cd - > /dev/null 2>&1
-      sudo rm -rf config
-      echo "Erased"
-      read -p "Reboot system now? (y/n)" REBOOTNOW
-      if [[ $REBOOTNOW == "y" || $REBOOTNOW == "Y" || $REBOOTNOW == "yes" || $REBOOTNOW == "Yes" ]]; then
-        echo "Rebooting..."
-        sudo reboot
-      fi
-    else
+install_config() {
+  ask "Are you sure you want to continue? <y/n> " || return
+  echo "Unpacking..."
+  mv config.tar.gz config > /dev/null 2>&1
+  cd config > /dev/null 2>&1
+  sudo tar -pxzf config.tar.gz > /dev/null 2>&1
+  rm -r config.tar.gz > /dev/null 2>&1
+  echo "Installing..."
+  cd - > /dev/null 2>&1
+  cd config/sys_pref/tmp
+  sudo tar -pxzf preferences.tar.gz > /dev/null 2>&1
+  rm -f preferences.tar.gz
+  sudo rsync -av -I --progress * /Library/Preferences/ > /dev/null 2>&1
+  cd - > /dev/null 2>&1
+  cd config/apps_pref/tmp
+  sudo tar -pxzf apps_preferences.tar.gz > /dev/null 2>&1
+  rm -f apps_preferences.tar.gz
+  sudo rsync -av -I --progress * ~/Library/Preferences/ > /dev/null 2>&1
+  cd - > /dev/null 2>&1
+  cd config > /dev/null 2>&1
+  sudo rsync -av -I --progress login.keychain ~/Library/Keychains/ > /dev/null 2>&1
+  echo "Installed"
+  cd - > /dev/null 2>&1
+  clean_config
+  if ask "Reboot system now? (y/n)"; then 
+    echo "Rebooting..."
+    sudo reboot
+  fi
+}
 
-    read -p "Reboot system now? (y/n)" REBOOTNOW
-    if [[ $REBOOTNOW == "y" || $REBOOTNOW == "Y" || $REBOOTNOW == "yes" || $REBOOTNOW == "Yes" ]]; then
-      echo "Rebooting..."
-      sudo reboot
-    fi
-fi
-fi
-fi
-
-
-
-
-if [ "$CHOICE" -eq 3 ]; then
-  read -p "Are you sure you want to recover default preferences? <y/n> " prompt
-  if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]
-  then
+recover_default_pref() {  
+  ask "Are you sure you want to recover default preferences? <y/n> " || return
   #rsync (hidden folder with .)
   echo "Done"
-  exit 0
-else
-  exit 0
-fi
-fi
+}
 
-if [ "$CHOICE" -eq 4 ]; then
-  read -p "Are you sure you want to clean config folder? <y/n> " prompt
-  if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]
-  then
+clean_config() {
+  ask "Are you sure you want to clean config folder? <y/n> " || return
   sudo rm -rf config
   echo "Done"
-  exit 0
-else
-  exit 0
-fi
+}
 
-fi
-#Config file, (or setup par -help for exmple). (What to config). Keychain backup, all settings.
-#
+cd "$(dirname "$0")"
+mkdir -p config/{sys,apps}_pref
+
+echo "CTRL+C to quit"
+select CHOISE in "create_config" "install_config" "restore_default_config" "clean_config" "exit"; do
+  case "$CHOISE" in
+    "create_config" | "install_config" | "restore_default_config" | "clean_config" | "exit" )
+      $CHOISE
+      ;;
+    * )
+      ask "Exit? <y/n>" && exit 0
+      ;;
+  esac
+done
